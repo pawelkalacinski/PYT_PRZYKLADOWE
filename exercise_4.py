e6493a00fb45a9a5e75cca289e1fdf0e8fa0be68
pawelkalacinski <pawel.kalacinski@gmail.com>
2019-02-14 20:52:33 +0100

    zadanie 4 - test1

---
+++ exercise_4.py
@@ -0,0 +1,23 @@
+
+
+def revers_sentence(rev):
+    out=rev[::-1]
+
+    return (out.title())
+
+
+
+
+
+
+
+
+
+
+
+
+rev = revers_sentence("Ala ma kota")
+print(rev)  # Atok Am Ala
+
+rev = revers_sentence("Python is easy")
+print(rev)  # Ysae Si Nohtyp
